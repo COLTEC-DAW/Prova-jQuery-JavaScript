6aa31a4acce7f31609e66573c6e281a737586396
Dener Vieira <deneribeiro10@hotmail.com>
2018-06-13 16:48:22 -0300

    layout (WIP)

--- js/share.js
+++ js/share.js
@@ -19,7 +19,6 @@ $.urlParam = function(name){
 	return results ? results[1] || 0 : 0;
 }
 if($.urlParam('bracket') != 0){
-    //console.log(JSON.parse(decodeURIComponent($.urlParam('bracket'))))
     const obj = JSON.parse(decodeURIComponent($.urlParam('bracket')))
     for(key in obj){
         const btn = $(`#${key}`)
