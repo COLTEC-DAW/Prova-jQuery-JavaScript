e92cd454d6c09a4a3f009f34af55ada312dee710
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2023-05-05 11:05:05 -0300

    Finalizei o JS

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let checkbox = document.querySelector("#check");
+let senha = document.querySelector("#senha");
+
+checkbox.addEventListener("change", () => {
+    
+    if(checkbox.checked){
+
+        senha.setAttribute("type", "text");
+
+    }else{
+
+        senha.setAttribute("type", "password");
+
+    }
+
+
+})
+
+
+
+
