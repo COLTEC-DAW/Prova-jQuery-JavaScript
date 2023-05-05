b64900647a358643880e303ab908da68d8e2795d
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2023-05-05 14:51:30 -0300

    Removi as mudanças

--- script.js
+++ script.js
@@ -25,15 +25,12 @@ let alert = document.querySelector(".alert-imp");
 
 button.addEventListener("click", () => {
 
-    if(alert.classList.lenght == 1){
-        alert.classList.toggle("alert-imp--active");
-        
-    } else {
-        alert.classList.toggle("alert-imp--active");
+    if(alert.classList.length == 1){
         alert.classList.toggle("alert-imp--active");
 
-        //alert.classList.remove("alert-imp--active");
-        //alert.classList.add("alert-imp--active");
+    } else {
+        alert.classList.remove("alert-imp--active");
+        alert.classList.add("alert-imp--active");
     }
     
 
