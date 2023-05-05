c8faec5c3c3f03b992e7f2f7a47dd1735c1d851e
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2023-05-05 13:33:19 -0300

    Up

--- script.js
+++ script.js
@@ -19,3 +19,25 @@ checkbox.addEventListener("change", () => {
 
 
 
+
+let button = document.querySelector(".botao");
+let alert = document.querySelector(".alert-imp");
+
+button.addEventListener("click", () => {
+
+    if(alert.classList.lenght == 1){
+        alert.classList.toggle("alert-imp--active");
+        
+    } else {
+        alert.classList.toggle("alert-imp--active");
+        alert.classList.toggle("alert-imp--active");
+
+        //alert.classList.remove("alert-imp--active");
+        //alert.classList.add("alert-imp--active");
+    }
+    
+
+
+})
+
+
