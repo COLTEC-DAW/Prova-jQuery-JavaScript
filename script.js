511822ceee759f9720f1da06927409c4edfac4e4
Erick Pedrosa <erick.pedrosa.b@gmail.com>
2023-05-05 15:18:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,25 +16,3 @@ checkbox.addEventListener("change", () => {
 
 })
 
-
-
-
-
-let button = document.querySelector(".botao");
-let alert = document.querySelector(".alert-imp");
-
-button.addEventListener("click", () => {
-
-    if(alert.classList.length == 1){
-        alert.classList.toggle("alert-imp--active");
-
-    } else {
-        alert.classList.remove("alert-imp--active");
-        alert.classList.add("alert-imp--active");
-    }
-    
-
-
-})
-
-
